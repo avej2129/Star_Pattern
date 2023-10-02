b4c51f54dd5b2c430256f6742e3b723bb9160e0b
Avej Shaikh <123964815+avej2129@users.noreply.github.com>
2023-10-02 19:32:57 +0530

    Add files via upload

---
+++ Star_Pattern/src/com/gl/StarPattern/Table.java
@@ -0,0 +1,17 @@
+package com.gl.StarPattern;
+
+public class Table {
+
+	public static void main(String[] args) {
+		
+		int Number = 50;
+		 
+	 for (int i = 1; i <= Number ; i++) {
+		 for (int j = 1; j <= 10  ; j ++) {
+			System.out.print(j*i+" ");
+		}
+		System.out.println();
+	}
+	}
+
+}
